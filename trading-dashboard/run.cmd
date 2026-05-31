d639f310e82b64dc08aabe8f6f6ffc2b5c6fccad
unknown <rose@roses.com>
2026-05-30 22:06:02 -0700

    Added Trading Dashboard with multiple indicators for Crypto and Equities

--- trading-dashboard/run.cmd
+++ trading-dashboard/run.cmd
@@ -1,6 +1,6 @@
 @echo on
 
-cd /d "D:\git_repo\trading-dashboard"
+cd /d "D:\git_repo\stockpile\trading-dashboard"
 call .venv\Scripts\activate
 
 echo "Starting Trading Dashboard at http://localhost:5000"
